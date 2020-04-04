fc2072639838f4f8ffeb37dd6c2b09aec0f5505a
qtw1998sjtuer <qtw1998sjtuer@gmail.com>
2020-04-05 00:47:34 +0800

    Update test and test circle ci

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -222,8 +222,34 @@ class EfficientNet(nn.Module):
                 list_feature.append(block._bn2.num_features)
         return list_feature[1:]
 
+from efficientnet_pytorch import EfficientNet
+class ModifyEfficientNet(EfficientNet):
+
+    def get_list_features(self):
+        list_feature = []
+        for idx, block in enumerate(self._blocks):
+            drop_connect_rate = self._global_params.drop_connect_rate
+
+            if(block._depthwise_conv.stride == [2, 2]):
+                list_feature.append(block._bn2.num_features)
+        return list_feature[1:]
+    def forward(self, inputs):
+        x = self._swish(self._bn0(self._conv_stem(inputs)))
+        P = []
+        # Blocks
+        for idx, block in enumerate(self._blocks):
+            drop_connect_rate = self._global_params.drop_connect_rate
+            if drop_connect_rate:
+                drop_connect_rate *= float(idx) / len(self._blocks)
+            x = block(x, drop_connect_rate=drop_connect_rate)
+            if(block._depthwise_conv.stride == [2, 2]):
+                P.append(x)
+        return P[1:]
+        
+
 if __name__ == '__main__':
-    model = EfficientNet.from_pretrained('efficientnet-b0')
+    # model = EfficientNet.from_pretrained('efficientnet-b0')
+    model = ModifyEfficientNet.from_pretrained('efficientnet-b0')
     inputs = torch.randn(4, 3, 640, 640)
     P = model(inputs)
     for idx, p in enumerate(P):
